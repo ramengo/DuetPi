3371d875bd033ceef7a9a16f8ef932a5b3b45423
ramengo <angelocantalp@gmail.com>
2025-04-18 16:05:56 +0200

    clean export stage that override the other

--- stage-dsf/07-mjpeg-install/01-run.sh
+++ stage-dsf/07-mjpeg-install/01-run.sh
@@ -3,7 +3,7 @@ install -m 755 files/mjpg_streamer "${ROOTFS_DIR}/usr/local/bin/mjpg_streamer"
 install -m 755 files/livestream.sh "${ROOTFS_DIR}/etc/init.d/livestream.sh"
 
 on_chroot << EOF
-	chown root:root /etc/init.d/livestream.sh
+	chown pi:pi /etc/init.d/livestream.sh
 	chmod 755 /etc/init.d/livestream.sh
 	update-rc.d livestream.sh defaults
 EOF
