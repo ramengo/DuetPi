5c8cf8bd81f0bbb1777441cf9bf81b887b3046b7
ramengo <angelocantalp@gmail.com>
2025-04-28 11:22:41 +0200

    update mjpeg streamer stage on tc producition working

--- stage4/06-mjpeg-install/01-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-install -m 755 files/mjpg_streamer "${ROOTFS_DIR}/usr/local/bin/mjpg_streamer"
-install -m 755 files/livestream.sh "${ROOTFS_DIR}/etc/init.d/livestream.sh"
-
-on_chroot << EOF
-	chown pi:pi /etc/init.d/livestream.sh
-	chmod 755 /etc/init.d/livestream.sh
-	update-rc.d livestream.sh defaults
-EOF
